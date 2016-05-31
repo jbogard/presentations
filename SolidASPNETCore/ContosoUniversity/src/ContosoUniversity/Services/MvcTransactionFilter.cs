71daf2040974bde044c29f743f0555abe565c34f
Jimmy Bogard <jimmy.bogard@gmail.com>
2016-05-31 08:12:23 -0500

    Solid Aspnet core

---
+++ SolidASPNETCore/ContosoUniversity/src/ContosoUniversity/Services/MvcTransactionFilter.cs
@@ -0,0 +1,39 @@
+﻿namespace ContosoUniversity.Services
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc.Filters;
+    using Models;
+
+    public class MvcTransactionFilter : IAsyncActionFilter
+    {
+        private readonly SchoolContext _dbContext;
+
+        public MvcTransactionFilter(SchoolContext dbContext)
+        {
+            _dbContext = dbContext;
+
+
+        }
+         
+        public async Task OnActionExecutionAsync(
+            ActionExecutingContext context, 
+            ActionExecutionDelegate next)
+        {
+            try
+            {
+                await _dbContext.BeginTransaction();
+
+                await next();
+
+                await _dbContext.CloseTransaction();
+            }
+            catch (Exception e)
+            {
+                await _dbContext.CloseTransaction(e);
+
+                throw;
+            }
+        }
+    }
+}
---
+++ SolidASPNETCore/ContosoUniversity/src/ContosoUniversity/ViewModels/StudentModels.cs
@@ -0,0 +1,49 @@
+﻿namespace ContosoUniversity.ViewModels
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using Models;
+    using X.PagedList;
+    using AutoMapper;
+
+    public class StudentIndexQuery
+    {
+        public string SortOrder { get; set; }
+        public string CurrentFilter { get; set; }
+        public string SearchString { get; set; }
+        public int? Page { get; set; }
+    }
+
+    public class StudentIndexResult
+    {
+        public string CurrentSort { get; set; }
+        public string NameSortParm { get; set; }
+        public string DateSortParm { get; set; }
+        public string CurrentFilter { get; set; }
+        public string SearchString { get; set; }
+
+        public IPagedList<StudentIndexModel> Results { get; set; }
+    }
+
+    public class StudentIndexModel
+    {
+        public int ID { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstMidName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+    }
+
+    public class StudentMappingProfile : Profile
+    {
+        public StudentMappingProfile()
+        {
+            CreateMap<Student, StudentIndexModel>();
+        }
+    }
+
+}
